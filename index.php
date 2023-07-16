f8346b30aa8ae1c37af0b34decf93404d9da3773
Philip <philiposun@gmail.com>
2023-07-16 01:26:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,6 +25,21 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $database = "myshop";
+
+                    // Create connection
+                    $conn = new mysqli($servername, $username, $password, $database);
+
+                    // Check connection
+                    if ($conn->connect_error) {
+                        die("Connection failed " . $conn->connect_error);
+                    }
+
+                    ?>
                     <tr>
                         <td>10</td>
                         <td>Bill Gate</td>
