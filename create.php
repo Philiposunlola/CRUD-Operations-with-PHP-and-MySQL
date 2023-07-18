5ff9584054a944da549050654f4b76fefa71b829
Philip <philiposun@gmail.com>
2023-07-18 01:53:21 +0100

    Update create.php

--- create.php
+++ create.php
@@ -27,6 +27,7 @@
             $address = "";
 
             $successMessage = "Client added correctly";
+            break;
 
         } while (false);
             
@@ -40,7 +41,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA Compatible" content="IE-edge">
         <meta name="viewpoint" content="width-device-width", initial-scale-1.0>
-        <title>My Shop</title>
+        <title>My Store</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     </head>
