be2a819b10dceff7a50a56623e63ef51194f713a
Philip <philiposun@gmail.com>
2023-07-19 01:35:48 +0100

    Update edit.php

---
+++ edit.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA Compatible" content="IE-edge">
+        <meta name="viewpoint" content="width-device-width", initial-scale-1.0>
+        <title>My Store</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <div class="container my-5">
+            <h2>New Client</h2>
+
+            <?php
+                if (!empty($errorMessage)) {
+                    echo "
+                    <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                        <strong>$errorMessage</strong>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='close'></button>
+                    </div>
+                    "; 
+                }
+            ?>
+
+            <form method="POST">
+                <div class="row mb-3">
+                    <label class="col-sm-3 col-form-label">Name</label>
+                    <div class="col-sm-6">
+                        <input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label class="col-sm-3 col-form-label">Email</label>
+                    <div class="col-sm-6">
+                        <input type="text" class="form-control" name="email" value="<?php echo $email; ?>">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label class="col-sm-3 col-form-label">Phone</label>
+                    <div class="col-sm-6">
+                        <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label class="col-sm-3 col-form-label">Address</label>
+                    <div class="col-sm-6">
+                        <input type="text" class="form-control" name="address" value="<?php echo $address; ?>">
+                    </div>
+                </div>
+
+                <?php
+                    if (!empty($successMessage)) {
+                        echo "
+                        <div class='row mb-3'>
+                            <div class='offset-sm-3 col-sm-6'>
+                                <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                                    <strong>$successMessage</strong>
+                                    <button type='button' class='data-bs-dismiss='alert' aria-label='close'></button>
+                                </div>
+                            </div>
+                        </div>
+                        "; 
+                    }
+                ?>
+
+                <div class="row mb-3">
+                    <div class="offset-sm-3 col-sm-3 d-grid">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                    <div class="col-sm-3 d-grid">
+                        <a class="btn btn-outline-primary" href="/CRUD Operations-with-PHP-and-MySQL/index.php" role="button">Cancel</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>  
