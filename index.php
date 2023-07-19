18d01a992f0e6b84cde7d7e659f4a485b38e3499
Philip <philiposun@gmail.com>
2023-07-19 01:13:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
         <meta name="viewpoint" content="width-device-width", initial-scale-1.0>
         <title>My Store</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
         <div class="container my-5">
