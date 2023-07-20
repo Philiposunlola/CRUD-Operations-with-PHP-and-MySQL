8fe01503c4295f4cdd4f72fe2ae46e9dabfbeaf9
Philip <philiposun@gmail.com>
2023-07-20 01:02:40 +0100

    CRUD

--- edit.php
+++ edit.php
@@ -43,8 +43,37 @@
     }
     else {
         // POST Method: update the data of the client
-    }
 
+        $id = $_POST["id"];
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $address = $_POST["address"];
+
+        do {
+            if (empty($id) || empty($name) || empty($email) || empty($phone) || empty($address)) {
+            $errorMessage = "All the fields are required";
+            break;
+            }
+
+            $sql = "UPDATE clients " .  
+                    "SET name = '$name', email = '$email', phone = '$phone', address = '$address' " .
+                    "WHERE id = $id";
+
+            $result = $conn->query($sql);
+
+            if (!$result) {
+                $errorMessage = "Invalid query: " . $conn->error;
+                break;
+            }
+
+            $successMessage = "client updated correctly";
+
+            header("location: /CRUD Operations-with-PHP-and-MySQL/index.php");
+            exit;
+
+        } while (false);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -73,7 +102,7 @@
             ?>
 
             <form method="POST">
-                <input type="hidden" value="<?php echo $id; ?>">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
                 <div class="row mb-3">
                     <label class="col-sm-3 col-form-label">Name</label>
                     <div class="col-sm-6">
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA Compatible" content="IE-edge">
         <meta name="viewpoint" content="width-device-width", initial-scale-1.0>
         <title>My Store</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     </head>
     <body>
         <div class="container my-5">
@@ -26,44 +26,44 @@
                 </thead>
                 <tbody>
                     <?php
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "";
-                    $database = "myshop";
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $database = "myshop";
 
-                    // Create connection
-                    $conn = new mysqli($servername, $username, $password, $database);
+                        // Create connection
+                        $conn = new mysqli($servername, $username, $password, $database);
 
-                    // Check connection
-                    if ($conn->connect_error) {
-                        die("Connection failed " . $conn->connect_error);
-                    }
+                        // Check connection
+                        if ($conn->connect_error) {
+                            die("Connection failed " . $conn->connect_error);
+                        }
 
-                    // read all row from database table
-                    $sql = "SELECT * from clients";
-                    $result = $conn->query($sql);
+                        // read all row from database table
+                        $sql = "SELECT * from clients";
+                        $result = $conn->query($sql);
 
-                    if (!$result) {
-                        die("invalid query: " . $conn->error);
-                    }
+                        if (!$result) {
+                            die("invalid query: " . $conn->error);
+                        }
 
-                    // read data of each row
-                    while ($row = $result->fetch_assoc()) {
-                        echo "
-                        <tr>
-                            <td>$row[id]</td>
-                            <td>$row[name]</td>
-                            <td>$row[email]</td>
-                            <td>$row[phone]</td>
-                            <td>$row[address]</td>
-                            <td>$row[created_at]</td>
-                            <td>
-                                <a class='btn btn-primary btn-sm' href='CRUD Operations-with-PHP-and-MySQL/edit.php?id=$row[id]'>Edit</a>
-                                <a class='btn btn-danger btn-sm' href='CRUD Operations-with-PHP-and-MySQL/delete.php?id=$row[id]'>Delete</a>
-                            </td>
-                        </tr>
-                        ";
-                    }
+                        // read data of each row
+                        while ($row = $result->fetch_assoc()) {
+                            echo "
+                            <tr>
+                                <td>$row[id]</td>
+                                <td>$row[name]</td>
+                                <td>$row[email]</td>
+                                <td>$row[phone]</td>
+                                <td>$row[address]</td>
+                                <td>$row[created_at]</td>
+                                <td>
+                                    <a class='btn btn-primary btn-sm' href='CRUD Operations-with-PHP-and-MySQL/edit.php?id=$row[id]'>Edit</a>
+                                    <a class='btn btn-danger btn-sm' href='CRUD Operations-with-PHP-and-MySQL/delete.php?id=$row[id]'>Delete</a>
+                                </td>
+                            </tr>
+                            ";
+                        }
                     ?>
                 </tbody>
             </table>
